61d0801b881373f7e3b61674b67489daf61b10d8
Ronja Norlén <rono2300@student.miun.se>
2024-05-16 12:01:20 +0200

    Genererat komponent för campus

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { CoursesComponent } from './pages/courses/courses.component';
 import { RamschemaComponent } from './pages/ramschema/ramschema.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { StudentUnionComponent } from './pages/student-union/student-union.component';
+import { CampusComponent } from './pages/campus/campus.component';
 
 export const routes: Routes = [
     { path: "home", component: HomeComponent },
     { path: "courses", component: CoursesComponent },
     { path: "ramschema", component: RamschemaComponent },
     { path: "student-union", component: StudentUnionComponent },
+    { path: "campus", component: CampusComponent },
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "404", component: NotFoundComponent },
     { path: "**", component: NotFoundComponent }
---
+++ src/app/pages/campus/campus.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-campus',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './campus.component.html',
+  styleUrl: './campus.component.scss'
+})
+export class CampusComponent {
+  // Property för bild
+  campusImage: string = "./assets/images/campus2.jpg";
+}
